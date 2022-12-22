d7ed72dd66f2282fc876ba561f0084ea125d2e6c
NickAssassin666 <84249423+NickAssassin666@users.noreply.github.com>
2022-12-23 01:16:27 +0300

    third commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/header/Header";
 import AboutSection from "../components/about/AboutSection";
 import ExperienceSection from "../components/experience/ExperienceSecion";
-import ProjectSection from "../components/projects/ProjectSection";
+import ProjectSection from "../components/Projects/ProjectSection";
 import CtaSection from "../components/cta/CtaSection";
 import Footer from "../components/Footer";
 
